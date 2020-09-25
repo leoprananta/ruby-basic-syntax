3cbd19e91a8dc185f9603bb398ee702d94a85364
leoprananta <leonantapramudya7@gmail.com>
2020-09-25 17:45:01 +0700

    add array

---
+++ array.rb
@@ -0,0 +1,19 @@
+#array in ruby
+
+angka = [1, 2, 3]
+
+bahasa = ["html", "css", "js", "php"]
+
+puts(bahasa)
+
+puts("=======================================")
+
+puts(bahasa.first)
+puts(bahasa.last)
+
+puts("=======================================")
+
+puts "2 pertama : #{bahasa.take(2)}"
+puts "2 terakhir : #{bahasa.drop(2)}"
+puts "Jumlah : #{bahasa.count}"
+puts "Length  : #{bahasa.length}"
